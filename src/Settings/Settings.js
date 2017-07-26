8e75e12443df86b3446f47649151c15fa03538b3
daniel-fitzgerald <Fitzgerald Daniel@agdaff.gov.au>
2017-07-26 10:46:39 +1000

    added tile thing

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -10,8 +10,21 @@ const breadcrumbs = {
 }
 
 let Settings = ({ location }) => <Content title="Settings" secondaryNav={SecondaryNav} location={location} breadcrumbs={breadcrumbs}>
-    <ul>
+    <ul className="landing-page-links biosecurity-bg">
+        <li><Link to="/settings/cut-suffix">Cut Suffix</Link></li>
+        <li><Link to="/settings/cut-code">Cut Code</Link></li>
+        <li><Link to="/settings/tariff-code">Tariff Code</Link></li>
+        <li><Link to="/settings/commodity">Commodity</Link></li>
+        <li><Link to="/settings/quota">Qutoa</Link></li>
         <li><Link to="/settings/exporter">Exporter</Link></li>
+        <li><Link to="/settings/importer">Importer</Link></li>
+        <li><Link to="/settings/destination">Destination</Link></li>
+        <li><Link to="/settings/discharge-port">Discharge Port</Link></li>
+        <li><Link to="/settings/load-port">Load Port</Link></li>
+        <li><Link to="/settings/works">Works</Link></li>
+        <li><Link to="/settings/Works Accreditation">Works Accreditation</Link></li>
+        <li><Link to="/settings/Office Locations">Office Locations</Link></li>
+        <li><Link to="/settings/Global Exports">Global Exports</Link></li>
     </ul>
 </Content>
 
